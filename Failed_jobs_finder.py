802e3bb570d7e55000a3eebfb805f9189b89e86e
alons126 <alonsportes@mail.tau.ac.il>
2024-08-21 14:23:24 +0300

    Branch creation

--- Failed_jobs_finder.py
+++ Failed_jobs_finder.py
@@ -1,20 +1,18 @@
 import os
 
 
-def find_unmatched_scripts(output_txt):
-    PrinOut = False
+def find_unmatched_scripts(prefix, input_txt, output_txt, false=None):
+    PrinOut = false
 
-    dir = os.getenv("PyDir")
-    list_file = os.getenv("List_File")
-    # os.system("ls " + dir + "master-routine_validation_01-eScattering >" + dir + list_file)
-    # os.system("ls -1 . >" + list_file)
+    list_file = "list.txt"
+    os.system("ls -1 . >" + list_file)
 
     # Obtain prefix
-    with open(dir + "/grid_submission.xml", 'r') as xml_file:
+    with open("grid_submission.xml", 'r') as xml_file:
         for xml_line in xml_file:
             if "<parallel>" not in xml_line:
                 prefix_index = xml_line.rfind('/')
-                prefix = xml_line[:prefix_index] + "/"
+                prefix0 = xml_line[:prefix_index] + "/"
                 break
 
     xml_file.close()
@@ -24,8 +22,8 @@ def find_unmatched_scripts(output_txt):
     sh_files = []
 
     # Read the input .txt file
-    with open(dir + "/" + list_file, 'r') as file:
-        # with open(input_txt, 'r') as file:
+    with open(list_file, 'r') as file:
+    # with open(input_txt, 'r') as file:
         for line in file:
             line = line.strip()
             if line.endswith('.gst.root'):
@@ -69,8 +67,6 @@ def find_unmatched_scripts(output_txt):
             file.write(prefix + sh_file + '\n')
         file.write('</parallel>\n')
 
-    os.system("cp " + output_txt + " " + dir + "/" + output_txt)
-
     print("Expected Number of '.gst.root' and '.sh' files: " + str(len(sh_files) * 2))
     print("Number of unmatched '.sh' files: " + str(len(unmatched_sh_files)))
 
@@ -78,4 +74,4 @@ def find_unmatched_scripts(output_txt):
 if __name__ == "__main__":
     input_txt = "input.txt"  # List of files in master-routine_validation_01-eScattering
     output_txt = "grid_submission.xml"  # output .xml file
-    find_unmatched_scripts(output_txt)
+    find_unmatched_scripts(prefix, input_txt, output_txt)
